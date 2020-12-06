cd326dbe288c6c0d1790408c4590734186c572e4
Emily Davies <emily@daviesland.net>
2020-12-05 22:33:04 -0500

    - added menu bar item *test* - after our 12/5/20 meeeting

--- app/src/main/java/com/example/app/fragment_test.java
+++ app/src/main/java/com/example/app/fragment_test.java
@@ -1,64 +1,37 @@
 package com.example.app;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_test#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+//import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.app.R;
+import com.example.app.ui.TestViewModel;
+
 public class fragment_test extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TestViewModel testViewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public fragment_test() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_test.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_test newInstance(String param1, String param2) {
-        fragment_test fragment = new fragment_test();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        testViewModel =
+                new ViewModelProvider(this).get(TestViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_test, container, false);
+        final TextView textView = root.findViewById(R.id.testTextView);
+        testViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 }
---
+++ app/src/main/java/com/example/app/ui/TestViewModel.java
@@ -0,0 +1,19 @@
+package com.example.app.ui;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class TestViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public TestViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is test fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
