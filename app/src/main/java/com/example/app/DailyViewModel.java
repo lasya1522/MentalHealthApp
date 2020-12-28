4701a3a4b4015617de920d97765031f962f2cdf2
Emily Davies <emily@daviesland.net>
2020-12-27 22:59:20 -0500

    restored 4th and 5th screens imported graph library & started looking at it

---
+++ app/src/main/java/com/example/app/DailyViewModel.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DailyViewModel extends ViewModel {
+
+   // private MutableLiveData<String> mText;
+
+    public DailyViewModel() {
+       // mText = new MutableLiveData<>();
+        //mText.setValue("This is daily life fragment");
+    }
+
+   // public LiveData<String> getText() {
+      //  return mText;
+   // }
+}
