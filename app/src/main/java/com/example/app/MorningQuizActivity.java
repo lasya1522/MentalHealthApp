4331a1ad340274530d46287a22a8cd6230c4ffc0
Emily Davies <emily@daviesland.net>
2020-12-29 15:31:30 -0500

    fixed error on home screen to daily quiz button

--- app/src/main/java/com/example/app/MorningQuizActivity.java
+++ app/src/main/java/com/example/app/MorningQuizActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NightQuiz extends AppCompatActivity {
+public class MorningQuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_night_quiz);
+        setContentView(R.layout.activity_morning_quiz);
     }
 }
