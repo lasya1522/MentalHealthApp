a423fb12e58b3364ca57c7edabd73119c0ea6a9f
Emily Davies <emily@daviesland.net>
2021-01-25 17:43:39 -0500

    set up test UI for the view past quizzes screen and set up so that we can pull a specfic day's quiz from the database to view according to the datepicker

--- app/src/main/java/com/example/app/ViewPastQuizActivity.java
+++ app/src/main/java/com/example/app/ViewPastQuizActivity.java
@@ -16,6 +16,14 @@ import java.util.List;
 public class ViewPastQuizActivity extends AppCompatActivity {
     TextView tv_date;
     TextView tv_mood;
+    TextView tv_sleepTime;
+    TextView tv_sleepRating;
+    TextView tv_productiveTime;
+    TextView tv_relaxTime;
+    TextView tv_exerciseTime;
+    TextView tv_stressLevel;
+    TextView tv_stressors;
+    TextView tv_other;
 
     List<DailyQuiz> list;
     DailyQuiz dailyQuiz;
@@ -30,6 +38,14 @@ public class ViewPastQuizActivity extends AppCompatActivity {
 
         tv_date = findViewById(R.id.tv_date);
         tv_mood = findViewById(R.id.tv_mood);
+        tv_sleepTime = findViewById(R.id.tv_sleepTime);
+        tv_sleepRating = findViewById(R.id.tv_sleepRating);
+        tv_productiveTime = findViewById(R.id.tv_productiveTime);
+        tv_relaxTime = findViewById(R.id.tv_relaxTime);
+        tv_exerciseTime = findViewById(R.id.tv_exerciseTime);
+        tv_stressLevel = findViewById(R.id.tv_stressLevel);
+        tv_stressors = findViewById(R.id.tv_stressors);
+        tv_other = findViewById(R.id.tv_other);
 
         Intent intent = getIntent();
         String passedDate = intent.getStringExtra("date");
@@ -37,7 +53,16 @@ public class ViewPastQuizActivity extends AppCompatActivity {
         DatabaseHelper databaseHelper = new DatabaseHelper(this);
         dailyQuiz = databaseHelper.getDailyQuiz(passedDate);
         tv_date.setText(passedDate);
-        tv_mood.setText(dailyQuiz.getMood());
+        tv_mood.setText("Mood: " + dailyQuiz.getMood());
+        tv_sleepTime.setText("sleepTime: " + dailyQuiz.getSleepTime().toString());
+        tv_sleepRating.setText("sleepRating: " + dailyQuiz.getSleepRating());
+        tv_productiveTime.setText("productiveTime: " + dailyQuiz.getProductiveTime().toString());
+        tv_relaxTime.setText("relaxTime: " + dailyQuiz.getRelaxTime().toString());
+        tv_exerciseTime.setText("exerciseTime: " + dailyQuiz.getExerciseTime().toString());
+        tv_stressLevel.setText("stressLevel: " + dailyQuiz.getStressLevel());
+        tv_stressors.setText("stressors: " + dailyQuiz.getStressors());
+        tv_other.setText("other: " + dailyQuiz.getOther());
+
 
 
     }
