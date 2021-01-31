86feb765630378096e6e0fb2dd2d074dea992f9f
Emily Davies <emily@daviesland.net>
2021-01-31 16:22:37 -0500

    tried to set up the alarm

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,31 +1,45 @@
 package com.example.app;
 
 import android.app.AlarmManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
+import com.example.app.ui.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+    SettingsFragment sf;
+    TextView tv_viewAlarmTime;
     private Button button; //Daily quiz button
+    public static Calendar c; // public static?
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // sf = (SettingsFragment) this.getSupportFragmentManager().findFragmentByTag("fragment_settings");
+
+        tv_viewAlarmTime = findViewById(R.id.tv_viewAlarmTime);
+      //  c = Calendar.getInstance();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -45,6 +59,44 @@ public class MainActivity extends AppCompatActivity {
     public void browser1(View view) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://suicidepreventionlifeline.org/talk-to-someone-now/"));
         startActivity(browserIntent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+       // updateTimeText(c);
+       // startAlarm(c);
+      //  Toast.makeText(this, sf.toString(), Toast.LENGTH_LONG).show();
+      //  sf = (SettingsFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_settings);
+      //  sf.updateTimeText(c);
+      //  sf.startAlarm(c);
+    }
+
+    public void updateTimeText(Calendar c) {
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+       // SettingsFragment sf = (SettingsFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_settings);
+      // tv_viewAlarmTime.setText(timeText);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT) // I don't know what this means... it can't run on old versions, I think
+    public void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+        if (c.before(Calendar.getInstance())) {
+            c.add(Calendar.DATE, 1);
+        }
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+    }
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+        alarmManager.cancel(pendingIntent);
+       // tv_viewAlarmTime.setText("Alarm canceled");
     }
 
 }
--- app/src/main/java/com/example/app/ui/SettingsFragment.java
+++ app/src/main/java/com/example/app/ui/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
@@ -12,35 +13,56 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.app.AlertReceiver;
 import com.example.app.DatabaseHelper;
+import com.example.app.JournalEntry;
+import com.example.app.MainActivity;
 import com.example.app.R;
 import com.example.app.ReminderBroadcast;
+import com.example.app.TimePickerFragment;
 
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
 import java.util.Calendar;
 
 //import androidx.lifecycle.ViewModel;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment{
 
+    public TextView tv_viewAlarmTime;
     private SettingsViewModel settingsViewModel;
     private Button btn_clearData; // should the variables be private or not????????????
     DatabaseHelper databaseHelper;
 
-
-
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+       //getFragmentManager().beginTransaction().add, this, "fragment_settings").commit();
+
+
         settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
         btn_clearData = root.findViewById(R.id.btn_clearData);
         databaseHelper = new DatabaseHelper(this.getContext());
+
+        tv_viewAlarmTime = root.findViewById(R.id.tv_viewAlarmTime);
+        if (MainActivity.c != null) {
+            updateTimeText(MainActivity.c);
+            startAlarm(MainActivity.c);
+        }
 
         btn_clearData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +78,7 @@ public class SettingsFragment extends Fragment {
         //pretty sure that class is fine, its this place that has issues
         //-------------------------------------------
         // reminder button
-        createNotificationChannel();
+       // createNotificationChannel();
 
         //find button to use. btn_remind is on the settings page
         Button button_remind = root.findViewById(R.id.btn_remind);
@@ -64,7 +86,7 @@ public class SettingsFragment extends Fragment {
         //button can be found on settings page - fragment_settings.xml
 
 
-        button_remind.setOnClickListener(v -> {
+        /*button_remind.setOnClickListener(v -> {
             //toast says that user clicked the button.
             Toast.makeText(this.getContext(), "Reminder in one hour set!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this.getContext(), ReminderBroadcast.class);
@@ -90,12 +112,57 @@ public class SettingsFragment extends Fragment {
 
         });
 
+         */
+
+        Button buttonTimePicker = root.findViewById(R.id.button_timepicker);
+        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getFragmentManager(), "time picker"); // should it be a child fragment manager? used to be supportFragmentManager
+            }
+        });
+        Button buttonCancelAlarm = root.findViewById(R.id.button_cancel);
+        buttonCancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+
+
         return root;
 
     }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void updateTimeText(Calendar c) {
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+        tv_viewAlarmTime.setText(timeText);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT) // I don't know what this means... it can't run on old versions, I think
+    public void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) this.getActivity().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this.getContext(), AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getContext(), 1, intent, 0);
+        if (c.before(Calendar.getInstance())) {
+            c.add(Calendar.DATE, 1);
+        }
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+    }
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) this.getActivity().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this.getContext(), AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getContext(), 1, intent, 0);
+        alarmManager.cancel(pendingIntent);
+        tv_viewAlarmTime.setText("Alarm canceled");
+    }
+
+    }
+
 
     //compatibility method for lower java versions
-    private void createNotificationChannel(){
+  /*  private void createNotificationChannel(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "Willow";
             String description = "An hour ago you wanted a reminder to take your daily quiz!";
@@ -109,5 +176,6 @@ public class SettingsFragment extends Fragment {
 
     }
 
+   */
 
-}
+
